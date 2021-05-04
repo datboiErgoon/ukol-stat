4d7892b6d12bfd936af8b614c67eeb7bc02383a0
datboiErgoon <ardno.okper@seznam.cz>
2021-05-04 18:37:53 +0200

    Verze 5 - Stručné sepsání historie

---
+++ ukol-stat/js/historie.js
@@ -0,0 +1,171 @@
+const events= [
+    {
+        "year":"1500 př. n. l.",
+        "event":"Vznik Nocké kultury",
+        "detail":"Kultura Nok nebo Nocká kultura byla jednou z nejstarších civilizací subsaharské Afriky. Existovala v 1. tisíciletí př. n. l. ve střední a severozápadní části státu Nigérie. Je pojmenována podle vesnice Nok ve státě Kaduna, kde byly roku 1928 v cínovém dole objeveny zlomky terakotových sošek.",
+        "url":"https://cs.wikipedia.org/wiki/Kultura_Nok",
+     },
+     {
+         "year":"400 př. n. l.",
+         "event":"Rozvoj zpracování železa",
+         "detail":"Pozůstatky jednoduchých pecí na zpracování železa byly nalezeny na archeologickém nálezišti v Opi. Železná ruda byla tavena v přírodních tažných pecích a roztavená struska byla odváděna mělkými kanály, které poté vytvářely obrovské struskové bloky o hmotnosti 47 kg. Odhaduje se, že tyto pece tavily rudu při teplotách až 1450°C.",
+         "url":"https://en.wikipedia.org/wiki/Opi_(archaeological_site)",
+     },
+     {
+        "year":"1100",
+        "event":"Založení islámské říše Kanem",
+        "detail":"Kánem-bornuská říše byl středověký státní útvar, který existoval od 9. do konce 14. století v Africe v okolí Čadského jezera. Říše se rozkládala na území států Čad, Niger a Nigérie. Říši nejspíše založili kočovníci mluvící jazykem Teda-Daza. Mimo této skupiny však zde žili lidé z etnika Zaghawa, kteří postupně v říši získali převahu. Vládce, či spíše šaman, Sayfawa založil po roce 850 vládnoucí dynastii. Její představitele však nelze považovat za vládce, spíše se jednalo o nejvyšší duchovní představitele. Tradice poukazuje na to, že vládnoucí dynastie byla arabského původu a pocházela z rodu legendárního jemenského hrdiny Saífa ibn Dhí Jazana.",
+        "url":"https://cs.wikipedia.org/wiki/Kanem",
+     },
+     {
+         "year":"1807",
+         "event":"Zákon o obchodu s otroky 1807",
+         "detail":"Zákon o obchodu s otroky z roku 1807, oficiálně zákon o zrušení obchodu s otroky, byl zákon parlamentu Spojeného království zakazující obchod s otroky v Britském impériu. Ačkoli to nezrušilo otroctví, povzbudilo britské kroky, aby přiměly ostatní národní státy, aby zrušily své vlastní obchody s otroky.",
+         "url":"https://en.wikipedia.org/wiki/Slave_Trade_Act_1807",
+     },
+     {
+         "year":"1960",
+         "event":"Oddělení od Britů",
+         "detail":"V roce 1960 se oddělila Nigérie od Britů a získala nezávislost, v tentýž rok byla oficiálně schválena vlajka i státní znak a následně v roce 1963 se stala Nigérijskou republikou.",
+         "url":"https://en.wikipedia.org/wiki/Federation_of_Nigeria",
+
+     },
+     {
+         "year":"1999",
+         "event":"Ukončení vojenské vlády",
+         "detail":"Po smrti generála Abachy v roce 1998 byly v zemi konečně uspořádány svobodné a legitimní volby, prezidentem se na příštích osm let do roku 2007 stal Olusegun Obasanjo.",
+         "url":"https://cs.wikipedia.org/wiki/D%C4%9Bjiny_Nig%C3%A9rie",
+     },
+  ];
+
+
+
+const heroes = [
+    {
+       "name":"Sir Ahmadu Bello",
+       "birth":"12.6.1910",
+       "death":"15.1.1966",
+       "biography":"Ahmadu Ibrahim Bello, Sardauna ze Sokota, povýšen do šlechtického stavu jako Sir Ahmadu Bello, byl konzervativní nigerijský státník, který řídil Severní Nigérii přes nezávislost v roce 1960 a sloužil jako první a jediný premiér od roku 1954, až do jeho atentátu v roce 1966, během kterého dominoval národním záležitostem více než deset let. Byl také vůdcem Kongresu Severních Národů, vládnoucí strany v té době složené z elity Hausa-Fulani. Předtím byl zvolen do krajského zákonodárného sboru a později se stal ministrem vlády.",
+       "portraits":[
+          "ahmadubello1.jpg",
+          "ahmadubello2.jpg",
+          "ahmadubello3.jpg"
+       ],
+       "online":"https://en.wikipedia.org/wiki/Ahmadu_Bello"
+    },
+    {
+        "name":"Dr. Nnamdi Azikiwe",
+        "birth":"16.11.1904",
+        "death":"11.5.1996",
+        "biography":"Nnamdi Benjamin Azikiwe, obvykle označovaný jako „Zik“, byl nigerijský státník a politický vůdce, který sloužil jako první prezident Nigérie od roku 1963 do roku 1966. Považován za hybnou sílu národa, stal se známým jako „otec nigerijského nacionalismu“. Narodil se rodičům Igbo ze státu Anambra ve východní Nigérii v Zungeru. Jako mladý chlapec se naučil mluvit hausky (hlavní domorodý jazyk severní oblasti). Azikiwe byl později poslán žít se svou tetou a babičkou do Onitshy (jeho rodičovské vlasti), kde se naučil jazyk Igbo. Pobyt v Lagosu ho vystavil jazyku Yoruba; v době, kdy byl na vysoké škole, byl vystaven různým nigerijským kulturám a mluvil třemi jazyky (výhodou jako prezident).",
+        "portraits":[
+           "nnamdi1.jpg",
+           "nnamdi2.jpg",
+           "nnamdi3.jpg"
+        ],
+        "online":"https://en.wikipedia.org/wiki/Nnamdi_Azikiwe"
+     },
+     {
+        "name":"Funmilayo Ransome-Kuti",
+        "birth":"25.10.1900",
+        "death":"13.4.1978",
+        "biography":"Šéf Funmilayo Ransome-Kuti, rozená Frances Abigail Olufunmilayo Thomas, byla nigerijská pedagogka, politická aktivistka, suffragistka a aktivistka za práva žen. Narodila se v Abeokuta ve státě Ogun v Nigérii a byla první studentkou, která navštěvovala gymnázium Abeokuta. Jako dospělá pracovala jako učitelka, organizovala některé z prvních předškolních tříd v zemi a pořádala kurzy gramotnosti pro ženy s nízkými příjmy. Ve čtyřicátých letech založila „Ransome-Kuti Abeokuta Women’s Union“ a bojovala za práva žen. Požadovala lepší zastoupení žen v místních řídících orgánech a ukončení nespravedlivých daní na trhu. Popsaná médii jako „Lvice z Lisabí“, vedla pochody a protesty až 10 000 žen a přinutila vládnoucí Alake dočasně abdikovat v roce 1949. Zemřela ve věku 77 let poté, co byla zraněna při vojenském útoku na rodinný majetek.",
+        "portraits":[
+           "kuti1.jpg",
+           "kuti2.jpg",
+           "kuti3.jpg"
+        ],
+        "online":"https://en.wikipedia.org/wiki/Funmilayo_Ransome-Kuti"
+     },
+     {
+        "name":"Anthony Enahoro",
+        "birth":"22.7.1923",
+        "death":"15.12.2010",
+        "biography":"Šéf Anthony Eromosele Enahoro, byl jedním z předních nigerijských protikoloniálních a prodemokratických aktivistů. Narodil se jako nejstarší z deseti dětí v Uromi v dnešním nigerijském státě Edo. Jeho esanští rodiče byli Anastasius Okotako Enahoro a Fidelia Victoria Inibokun rozená Ogbidi Okojie. Enahoro má za sebou dlouhou a významnou kariéru v tisku, politice, veřejné službě a prodemokratickém hnutí. Enahoro, který byl vzděláván na vládní škole Uromi, vládní škole Owo a King's College v Lagosu, se stal v roce 1944 redaktorem novin Nnamdi Azikiwe, jižní nigerijský obránce Ibadan, ve věku 21 let, čímž se stal vůbec nejmladším nigerijským redaktorem. Později se stal pomocným redaktorem „West African Pilots“, „Lagos“, a šéfredaktorem „Morning Star“ v letech 1950 až 1953.",
+        "portraits":[
+           "anthony1.jpg",
+           "anthony2.jpg",
+           "anthony3.jpg"
+        ],
+        "online":"https://en.wikipedia.org/wiki/Anthony_Enahoro"
+     },
+     {
+         "name":"Kudirat Abiola",
+         "birth":"1951",
+         "death":"4.6.1996",
+         "biography":"Alhaja Kudirat Abiola, známá jednoduše jako Kudirat Abiola, byla nigerijská prodemokratická aktivistka. Byla zavražděna, zatímco její manžel Moshood Abiola byl zadržován nigerijskou vládou. Byla vítěznou kandidátkou ve volbách, které se konaly v Nigérii v roce 1993, a byla zatčena krátce poté.",
+         "portraits":[
+            "kudirat1.jpg",
+            "kudirat2.jpg",
+            "kudirat3.jpg"
+         ],
+         "online":"https://en.wikipedia.org/wiki/Kudirat_Abiola"
+      },
+      {
+         "name":"Obafemi Awolowo",
+         "birth":"6.3.1909",
+         "death":"9.5.1987",
+         "biography":"Šéf Obafemi Jeremiah Oyeniyi Awolowo byl nigerijský nacionalista a státník, který hrál klíčovou roli v hnutí za nezávislost Nigérie, v První a Druhé republice a v Občanské válce. Byl synem jorubského farmáře a byl jedním ze skutečně self-made mužů mezi svými současníky v Nigérii. Jako mladý muž byl aktivním novinářem a kromě jiných redigoval publikace, třeba jako „Nigerijský dělník“. oté, co získal bakalářský titul v Nigérii, odcestoval do Londýna, kde získal titul v oboru práva. Obafemi Awolowo byl prvním premiérem západního regionu a později federálním komisařem pro finance a místopředsedou Federální výkonné rady. Během nigerijské Občanské války byl třikrát hlavním uchazečem o nejvyšší úřad ve své zemi.",
+         "portraits":[
+            "awolowo1.jpg",
+            "awolowo2.jpg",
+            "awolowo3.jpg"
+         ],
+         "online":"https://en.wikipedia.org/wiki/Obafemi_Awolowo"
+      },
+      {
+        "name":"Margaret Ekpo",
+        "birth":"27.7.1914",
+        "death":"21.8.2006",
+        "biography":"Šéfka Margaret Ekpo byla nigerijská aktivistka za práva žen a sociální mobilizátorka, která byla průkopnickou političkou v První republice a přední členkou třídy tradičních nigerijských aktivistek, z nichž mnohé shromáždil ženy nad rámec představ o etnické solidaritě. Hrála hlavní roli jako občanská a nacionalistická politička ve východonigerijském městě Aba, v době hierarchického a mužem ovládaného hnutí za nezávislost, s jejím vzestupem v neposlední řadě pomohla socializace role žen do role pomocnic nebo příplatky ke kariéře mužů.",
+        "portraits":[
+           "ekpo1.jpg",
+           "ekpo2.jpg",
+           "ekpo3.jpg"
+        ],
+        "online":"https://en.wikipedia.org/wiki/Margaret_Ekpo"
+     },
+     {
+        "name":"Herbert Macaulay",
+        "birth":"14.11.1864",
+        "death":"7.5.1946",
+        "biography":"Olayinka Herbert Samuel Heelas Badmus Macaulay byl nigerijský nacionalista, politik, zeměměřič, inženýr, architekt, novinář a hudebník a je považován za zakladatele nigerijského nacionalismu. Narodil se v Broad St., Lagos dne 14. listopadu 1864 v rodině Thomase Babingtona Macaulaye a Abigail Crowtherové. Jeho rodiči byly děti lidí zajatých z dnešní Nigérie, přesídlených v Sierra Leone britskou letkou ze západní Afriky a případní navrátilci do dnešní Nigérie.",
+        "portraits":[
+           "herbert1.jpg"
+        ],
+        "online":"https://en.wikipedia.org/wiki/Herbert_Macaulay"
+     },
+ ];
+
+
+
+
+ const articles = [
+    {
+       "title":"Cenné sochy na cestě zpět do Afriky. Německo se chystá napravit křivdy kolonialismu",
+       "text":"Evropské kulturní instituce mění pohled na umění odvezené z bývalých afrických kolonií. Německá i britská muzea nejspíš vrátí zpět do Nigérie stovky cenných bronzových soch ukradených v 19. století. Artefakty, takzvaný beninský bronz, se nyní nacházejí ve většině evropských muzeí. Africké království Benin se rozkládalo na území dnešní Nigérie, v roce 1897 jej dobyli a jeho hlavní město vypálili Britové. A země se ocitla pod koloniální nadvládou. Dobyvatelé odtud do Evropy odvezli tisíce jedinečných bronzových soch, které pak ovlivnily i evropskou uměleckou avantgardu. Většinu z nich prodali do soukromých sbírek nebo muzeí.",
+       "date":"5.4.2021",
+       "author":"Dominika Perlínová",
+       "source":"https://zpravy.aktualne.cz/zahranici/ukradeny-beninsky-bronz/r~701222a2914e11ebb0fa0cc47ab5f122/",
+       "likes":"40",
+       "dislikes":"3",
+       "gallery":[
+          "sochy1.jpg",
+          "sochy2.jpg"
+       ]
+    },
+    {
+        "title":"",
+        "text":"",
+        "date":"",
+        "author":"",
+        "source":"",
+        "likes":"",
+        "dislikes":"",
+        "gallery":[
+           ".jpg",
+           ".jpg"
+        ]
+     },
+ ];
+
