66cfa3dbee467c7a3c620928881c32e970c3710a
datboiErgoon <ardno.okper@seznam.cz>
2021-05-19 01:07:32 +0200

    Verze 8 - Zhotovení serveru a přidání JSON

---
+++ server.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const fs = require('fs').promises;
+
+async function readJSON(path) {
+    const data = await fs.readFile(path)
+    .catch(err => console.error('Chyba načtení souboru: ', err));
+    return JSON.parse(data.toString());
+}
+
+const app = express();
+const server = http.createServer(app);
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/api/events', (req, res) => {
+    readJSON('data/udalosti.json')
+    .then(data => res.send(data))
+    .catch(err => res.send('Chyba lávky', err));
+});
+
+app.get('/api/events/:index', (req, res) => {
+    readJSON('/historie/data/udalosti.json')
+    .then(data => res.send(data[req.params.index]))
+    .catch(err => res.send('Chyba lávky', err));
+});
+
+const PORT = process.env.PORT || 8080;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
